28f178c08f811ac0315a37ae2b1a8d31b67216c5
Lucas Gonzalez <lucas.gonzalez.espinosa@outlook.com>
2024-04-18 16:29:22 -0400

    Update map component layout in map.component.html

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,20 +1,34 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import AOS from 'aos';
+// Initialization for ES Users
+import { Dropdown, Ripple, initTWE } from "tw-elements";
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styles: ``
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadHeader();
+    }
+  }
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
       AOS.init();
     }
   }
+
+  async loadHeader() {
+    // Importar el módulo de tw-elements solo si estamos en el navegador
+    const { Dropdown, Ripple, initTWE } = await import("tw-elements");
+    initTWE({ Dropdown, Ripple });
+  }
+
 }
