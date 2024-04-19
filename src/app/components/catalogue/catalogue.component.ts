431032859be1ca0ac393474ab30020be60bbc94f
Lucas Gonzalez <lucas.gonzalez.espinosa@outlook.com>
2024-04-18 20:08:49 -0400

    Update product prices and descriptions in catalogue.component.ts and update footer component layout in footer.component.html

--- src/app/components/catalogue/catalogue.component.ts
+++ src/app/components/catalogue/catalogue.component.ts
@@ -21,45 +21,45 @@ export class CatalogueComponent {
   products = [
     {
       name: 'Shampoo',
-      price: 100,
+      price: 5490,
       discount: 0,
       description: 'Repuesto Shampoo purificante Cabello Liso',
       image: 'https://production.na01.natura.com/on/demandware.static/-/Sites-natura-cl-storefront-catalog/default/dw80b4dc6a/2023-01-28/86944_1.jpg'
     },
     {
       name: 'Crema',
-      price: 200,
-      discount: 20,
+      price: 7560,
+      discount: 0,
       description: 'Repuesto crema nutritiva para cuerpo frambuesa y pimienta rosa - 400ml',
       image: 'https://production.na01.natura.com/on/demandware.static/-/Sites-natura-cl-storefront-catalog/default/dw28998b1d/2023-01-28/2819_1.jpg'
     },
     {
       name: 'jabón líquido',
-      price: 150,
-      discount: 15,
+      price: 7490,
+      discount: 0,
       description: 'Repuesto Jabon Liquido Corporal Avellana y Casis Tododia 300ml',
       image: 'https://production.na01.natura.com/on/demandware.static/-/Sites-natura-cl-storefront-catalog/default/dwfafda2e9/2023-01-28/86337_1.jpg'
     },
     {
       name: 'Desodorante',
-      price: 50,
+      price: 7590,
       discount: 0,
       description: 'Desodorante Roll-On Tododia Flor de Lis 70ml',
-      image: 'https://production.na01.natura.com/on/demandware.static/-/Sites-natura-cl-storefront-catalog/default/dw02c8b8b3/2023-01-28/73179_1.jpg'
+      image: 'https://production.na01.natura.com/on/demandware.static/-/Sites-natura-cl-storefront-catalog/default/dwed89616b/NATCHL-123131_1.jpg'
     },
     {
-      name: 'Protector Solar',
-      price: 300,
-      discount: 30,
-      description: 'Protector Solar Facial FPS 60 Chronos 50ml',
-      image: 'https://production.na01.natura.com/on/demandware.static/-/Sites-natura-cl-storefront-catalog/default/dw5e6b0f4a/2023-01-28/86338_1.jpg'
+      name: 'Shampoo',
+      price: 6790,
+      discount: 1670,
+      description: 'Shampoo Matizador Cabello Rubio o con Canas',
+      image: 'https://production.na01.natura.com/on/demandware.static/-/Sites-natura-cl-storefront-catalog/default/dwcf36691a/job_images/74053_1.jpg'
     },
     {
-      name: 'Perfume',
-      price: 250,
-      discount: 25,
-      description: 'Perfume Femenino Luna Intenso 75ml',
-      image: 'https://production.na01.natura.com/on/demandware.static/-/Sites-natura-cl-storefront-catalog/default/dw5e6b0f4a/2023-01-28/86338_1.jpg'
+      name: 'Crema',
+      price: 12990,
+      discount: 0,
+      description: 'Crema Nutritiva para el Cuerpo Tododia Frutas Rojas',
+      image: 'https://production.na01.natura.com/on/demandware.static/-/Sites-natura-cl-storefront-catalog/default/dwb1ca858e/2023-01-28/74900_1.jpg'
     }
   ]
 }
