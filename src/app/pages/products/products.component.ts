0d5b203d9b3a686c48254872d6d7d191f414e3d7
Lucas Gonzalez <lucas.gonzalez.espinosa@outlook.com>
2024-04-30 00:36:03 -0400

    Update map component layout in map.component.html

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import AOS from 'aos';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
+  }
 }
